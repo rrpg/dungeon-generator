494aaa533859540b3158e9bfcfc7ca58e0040eba
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-06-12 14:42:37 +0200

    main.c added

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dungeon.h"
+
+#define DUNGEON_WIDTH 10
+#define DUNGEON_HEIGHT 10
+
+int main()
+{
+    s_dungeon d;
+
+    d.width = d.height = DUNGEON_WIDTH;
+    d.grid = malloc(d.width * d.height);
+
+    d = generate_dungeon(d);
+    display_dungeon(d);
+
+    free(d.grid);
+
+    return 1;
+}
